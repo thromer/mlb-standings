30427ba6578d2a210cee0d8bcf891a3e3e61e2d7
Ted Romer <tromer@gmail.com>
2023-08-18 07:17:18 -0700

    remove extraneous (i think) pyi files

--- pyi/mlbstandings/spreadsheet.pyi
+++
@@ -1,7 +0,0 @@
-from _typeshed import Incomplete
-from typing import List, Tuple
-
-class Spreadsheet:
-    year: Incomplete
-    def __init__(self, year: int) -> None: ...
-    def newest_entry(self, sheet_name: str) -> Tuple[int, List[str]]: ...
