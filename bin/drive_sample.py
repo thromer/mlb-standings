45a1a3387de3f064d176c89386c947c5e8a3456f
Ted Romer <trome@gmail.com>
2023-08-19 18:39:42 -0700

    fix typo (google -> google_wrappers)

--- bin/drive_sample.py
+++ bin/drive_sample.py
@@ -4,5 +4,5 @@ import google.auth
 import mlbstandings.google_wrappers
 
 scopes=['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive.metadata.readonly']
-drive=mlbstandings.google.Drive(google.auth.default(scopes=scopes)[0])
+drive=mlbstandings.google_wrappers.Drive(google.auth.default(scopes=scopes)[0])
 print(drive.getSpreadsheetId('MLB Standings 2023'))
