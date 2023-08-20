7ae38d05253a99aabb3825e762d8ca0b144cd387
Ted Romer <tromer@gmail.com>
2023-08-20 14:20:43 -0700

    fetch test-data directory correctly

--- tests/test_baseballref.py
+++ tests/test_baseballref.py
@@ -1,10 +1,18 @@
+import os
+import pytest
+
 from fakes import FakeWeb
 from mlbstandings.baseballref import BaseballReference
 
 from datetime import datetime
 
 
-def testOpeningDay() -> None:
-    web = FakeWeb('test-data')  # TODO is relative path really ok?
+@pytest.fixture
+def testdatadir(request):
+    return os.path.join(request.fspath.dirname, "../test-data")
+
+
+def test_opening_day(testdatadir) -> None:
+    web = FakeWeb(testdatadir)
     bref = BaseballReference(web)
-    assert (bref.opening_day(datetime(2023, 1, 1)) == datetime(2023, 3, 30))
+    assert bref.opening_day(datetime(2023, 1, 1)) == datetime(2023, 3, 30)
