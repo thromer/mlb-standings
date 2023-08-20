fb9ebcedc6dc295dc19725099586b2ccb7e18c3d
Ted Romer <tromer@gmail.com>
2023-08-20 15:14:16 -0700

    test_baseballref.py type annotations

--- tests/test_baseballref.py
+++ tests/test_baseballref.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+from _pytest.nodes import Node
 
 from fakes import FakeWeb
 from mlbstandings.baseballref import BaseballReference
@@ -8,11 +9,11 @@ from datetime import datetime
 
 
 @pytest.fixture
-def testdatadir(request):
+def testdatadir(request: Node) -> str:
     return os.path.join(request.fspath.dirname, "../test-data")
 
 
-def test_opening_day(testdatadir) -> None:
+def test_opening_day(testdatadir: str) -> None:
     web = FakeWeb(testdatadir)
     bref = BaseballReference(web)
     assert bref.opening_day(datetime(2023, 1, 1)) == datetime(2023, 3, 30)
