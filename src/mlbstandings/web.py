9bce96959144cddc4cbe1ffa831f9cd42bc9c309
Ted Romer <tromer@gmail.com>
2024-03-22 19:32:44 -0700

    exponential backoff for web.py

--- src/mlbstandings/web.py
+++ src/mlbstandings/web.py
@@ -1,8 +1,15 @@
+import backoff
 import requests
-
 
 class Web:
     @staticmethod
+    @backoff.on_exception(
+        backoff.expo,
+        (requests.exceptions.HTTPError, requests.exceptions.ConnectionError, requests.exceptions.Timeout),
+        max_time=600,
+        giveup=lambda e : isinstance(e, requests.exceptions.HTTPError) and e.status_code not in set([429, 500, 503]),
+        max_value=60
+    )
     def read(url: str) -> str:
         print(f'Web.read({url}')
         r = requests.get(url)
