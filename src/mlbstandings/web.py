4093aa43111f48442ac4ab22fddb08dab5cacf73
Ted Romer <tromer@gmail.com>
2025-05-24 14:12:52 -0700

    aborted attempt to use urllib (no gzip though ...)

--- src/mlbstandings/web.py
+++ src/mlbstandings/web.py
@@ -1,19 +1,42 @@
 import backoff
 import requests
+import urllib.error
+import urllib.request
 
 class Web:
+    @staticmethod
+    def retryable(e):
+        code = None
+        if isinstance(e, requests.exceptions.HTTPError):
+            code = e.response.status_code
+        elif isinstance(e, urllib.error):
+            code = e.getcode()
+        if code is None:
+            return True
+        return code in set([429, 500, 503])        
+    
     @staticmethod
     @backoff.on_exception(
         backoff.expo,
         (requests.exceptions.HTTPError, requests.exceptions.ConnectionError, requests.exceptions.Timeout),
         max_time=600,
-        giveup=lambda e : isinstance(e, requests.exceptions.HTTPError) and e.response.status_code not in set([429, 500, 503]),
+        giveup=lambda e: not retryable(e),
         max_value=60
     )
     def read(url: str) -> str:
         print(f'Web.read({url}')
-        r = requests.get(url)
-        r.raise_for_status()
-        if r.encoding is None:
-            raise ValueError(f'Missing encoding in esponse to GET {url}')
-        return r.content.decode(r.encoding)
+        content = None
+        # TODO remove use of urllib if requests+urllib3 and baseball-reference.com ever play nice
+        if url.find('baseball-reference.com') > 0:
+            req = urllib.request.Request(url)
+            with urllib.request.urlopen(req) as f:
+                content = f.read()
+                encoding = 'ISO-8859-1'  # yowza, but I don't see it in the headers
+        else:
+            resp = requests.get(url)
+            resp.raise_for_status()
+            content = resp.content
+            encoding = resp.encoding
+        if encoding is None:
+            raise ValueError(f'Missing encoding in response to GET {url}')
+        return content.decode(encoding)
