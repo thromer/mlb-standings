4e4cacbe27851db1420ce12ac9e7a8bc84f0cc73
Ted Romer <tromer@gmail.com>
2025-06-04 22:22:26 -0700

    be specific about url for repro, just in case

--- src/local.py
+++ src/local.py
@@ -1,5 +1,5 @@
 import urllib3
 
 http = urllib3.PoolManager()
-resp = http.request('GET', 'https://www.baseball-reference.com/')
+resp = http.request('GET', 'https://www.baseball-reference.com/boxes/index.fcgi?year=2025&month=05&day=21')
 print(f'{resp.status=}')
