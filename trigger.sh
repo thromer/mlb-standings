0791e821c0cc1eb9f5c7d15852281f8e91968c7d
Ted Romer <tromer@gmail.com>
2025-07-02 11:07:48 -0700

    correct service name

--- trigger.sh
+++ trigger.sh
@@ -3,7 +3,7 @@
 PROJECT=mlb-standings-001
 PROJECT_NUMBER=$(gcloud projects describe $PROJECT --format="value(projectNumber)")
 LOCATION=us-west1
-SERVICE=unified-service
+SERVICE=mlb-standings-001-update
 APP=
 URL="https://${SERVICE}-${PROJECT_NUMBER}.${LOCATION}.run.app/${APP}"
 TOKEN="$(gcloud --project=${PROJECT} auth print-identity-token)"
