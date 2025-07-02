1a1d72934490870872773fa74ae20122aa9838f6
thromer <tromer@gmail.com>
2025-07-02 11:13:23 -0700

    Tidy up scripts (#4)

--- trigger.sh
+++ trigger.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-curl -X POST -H "Content-Type: application/json" -H "Authorization: bearer $(gcloud --project=mlb-standings-001 auth print-identity-token)" \
-     https://mlb-standings-001-update-432241755017.us-west1.run.app -d '{}'
+
+PROJECT=mlb-standings-001
+PROJECT_NUMBER=$(gcloud projects describe $PROJECT --format="value(projectNumber)")
+LOCATION=us-west1
+SERVICE=mlb-standings-001-update
+APP=
+URL="https://${SERVICE}-${PROJECT_NUMBER}.${LOCATION}.run.app/${APP}"
+TOKEN="$(gcloud --project=${PROJECT} auth print-identity-token)"
+
+curl -H "Content-Type: application/json" \
+     -H "Authorization: Bearer ${TOKEN}" \
+     "${URL}" -d '{}'
