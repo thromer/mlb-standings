21631631c65cf6b6ee9c567e54052a3a681f72e2
Ted Romer <tromer@gmail.com>
2026-01-25 22:20:55 -0800

    improved docker image ls invocation to find images to delete

--- deploy.sh
+++ deploy.sh
@@ -41,5 +41,5 @@ cd "$(realpath "$(dirname "${BASH_SOURCE[0]}")")" &&
 	   --cpu-boost \
 	   ${SERVICE} |& ts |& tee "${DEPLOY_LOG}" &&
     gcloud --project=${PROJECT} storage cp --gzip-local-all "${DEPLOY_LOG}" ${LOGS_BUCKET}/ &&
-    docker image ls -f "reference=${LOCATION}-docker.pkg.dev/${PROJECT}/artifacts/${SERVICE}*" |
-    	tail -n +2 | awk '$2 != "latest" {print $3}' | xargs -r docker image rm
+    docker image ls --format json -f "reference=${LOCATION}-docker.pkg.dev/${PROJECT}/artifacts/${SERVICE}*" |
+	jq -r 'select(.Tag != "latest") | .ID' | xargs -r echo docker image rm
